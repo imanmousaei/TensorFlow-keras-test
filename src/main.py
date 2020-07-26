697abcda9a61dc740a2a22d885ef2bfcc64f00b9
Iman Mousaei <imanmousaei1379@gmail.com>
2020-07-26 21:09:20 +0430

    added test trainingSamples

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
 from tensorflow import keras
 
+trainingSamples = []
+trainingLabels = []
+
+# [1,500] -> 0 , [501,1000] -> 1
+
+for i in range(1, 500):
+    trainingSamples.append(i)
+    trainingLabels.append(0)
+
+for i in range(501, 1000):
+    trainingSamples.append(i)
+    trainingLabels.append(1)
+
